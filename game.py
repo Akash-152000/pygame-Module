3a246538e71e6919b85f4f6b73a6afc3ff05127f
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-15 15:59:49 +0530

    Update game.py

--- game.py
+++ game.py
@@ -14,6 +14,17 @@ BLUE=(0,0,255)
 DISPLAYSURF.fill(WHITE)
 
 pygame.draw.polygon(DISPLAYSURF,GREEN,((146,0),(291,106),(236,277),(56,277),(0,106)))
+pygame.draw.line(DISPLAYSURF,BLUE,(220,330),(400,300))
+pygame.draw.circle(DISPLAYSURF,RED,(400,300),20,3)
+pygame.draw.rect(DISPLAYSURF,GREEN,(200,300,400,150),6)
+
+pixObj=pygame.PixelArray(DISPLAYSURF)
+pixObj[480][380]=BLACK
+pixObj[481][380]=BLACK
+pixObj[482][380]=BLACK
+pixObj[483][380]=BLACK
+pixObj[484][380]=BLACK
+del pixObj
 
 while True:
     for event in pygame.event.get():
